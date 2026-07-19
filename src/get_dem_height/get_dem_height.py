4ff3ed422ca85213adc75774f04c390ef7d33589
myslivets_max <by.mikky1206@gmail.com>
2026-07-19 15:22:29 +0300

    добавлена циклическая обработка в get_dem_height для непрерывной работы с получением отметок

--- src/get_dem_height/get_dem_height.py
+++ src/get_dem_height/get_dem_height.py
@@ -24,20 +24,24 @@ def main():
         raise Exception("DEM отсутствует")
 
     dem = chunk.elevation
+    while True:
+        try:
+            pnt = acad.doc.Utility.GetPoint()
 
-    pnt = acad.doc.Utility.GetPoint()
+            x = pnt[0]
+            y = pnt[1]
+            print("x, y =", x, y)
 
-    x = pnt[0]
-    y = pnt[1]
-    print("x, y =", x, y)
+            z = get_dem_height(dem, x, y)
+            print("z =", z)
 
-    z = get_dem_height(dem, x, y)
-    print("z =", z)
+            if z is None:
+                ms.app.messageBox("Точка находится вне границ ЦММ")
+                return
 
-    if z is None:
-        ms.app.messageBox("Точка находится вне границ ЦММ")
-        return
+            acad.model.AddText(f"{z:.2f}", APoint(x, y, z), 0.5)
 
-    acad.model.AddText(f"{z:.2f}", APoint(x, y, z), 0.5)
-
-    print(f"Создана отметка {z:.2f}")
+            print(f"Создана отметка {z:.2f}")
+        except Exception as e:
+            print(e)
+            break
